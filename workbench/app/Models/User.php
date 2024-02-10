bf4ffdd76073664bf387759f7b5d8e4843114c26
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-02-10 18:59:10 +0700

    chore(model): update workbench user model to comply the common uses of it
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- workbench/app/Models/User.php
+++ workbench/app/Models/User.php
@@ -32,6 +32,12 @@ class User extends Authenticatable implements HasCredentialTokens, HasDevices, H
 
     protected $fillable = ['name', 'email', 'password'];
 
+    protected $hidden = ['password', 'remember_token'];
+
+    protected $casts = [
+        'email_verified_at' => 'immutable_datetime',
+    ];
+
     protected static function newFactory()
     {
         return UserFactory::new();

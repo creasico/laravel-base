6ca9654f93ce91729340f1cef71ca39ef2b3b0ee
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-03-26 17:06:23 +0700

    chore: move `.env` files to `workbench` directory
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,14 +3,10 @@
 namespace Creasi\Tests;
 
 use Closure;
-use Creasi\Base\ServiceProvider;
-use Creasi\Nusa\ServiceProvider as NusaServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Laravel\Sanctum\SanctumServiceProvider;
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Workbench\App\Models\User;
-use Workbench\App\Providers\WorkbenchServiceProvider;
 
 abstract class TestCase extends Orchestra
 {
@@ -18,19 +14,6 @@ abstract class TestCase extends Orchestra
     use WithWorkbench;
 
     private ?User $currentUser = null;
-
-    /**
-     * @param  \Illuminate\Foundation\Application  $app
-     */
-    protected function getPackageProviders($app): array
-    {
-        return [
-            ServiceProvider::class,
-            NusaServiceProvider::class,
-            SanctumServiceProvider::class,
-            WorkbenchServiceProvider::class,
-        ];
-    }
 
     final protected function user(array|Closure $attrs = []): User
     {
